07a974f50005f9e59788d49f56659d6cf421207d
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-08-23 11:24:42 +0100

    fix a lot bugs

--- components/calender.tsx
+++ components/calender.tsx
@@ -1,25 +1,36 @@
-"use client"
+"use client";
 import { UserContext } from "@/context/user";
 import { useContext } from "react";
 import { PiCheckFatDuotone } from "react-icons/pi";
-import moment from "moment"
+import moment from "moment";
 export default function Calender() {
-  let {user,setUser} = useContext(UserContext)
+  let { user, setUser } = useContext(UserContext);
   return (
-    <div className="calender flex gap-6 flex-wrap mt-6"
-{
-  user.daysTasks.map(day=>(
-    <div key={day.day} className="flex-col gap-1 flex justify-start items-center">
-      <span className="text-gray-400 dark:text-gray-600 text-base">{moment(day.day).fromNow()}</span>
-      <div className={`rounded-full h-20 w-20 font-bold text-xl text-day flex justify-center items-center flex-col gap-1 ${user.filter(el=>!el.checked).legnth ==0?"bg-green-400":""}`}>
-        <PiCheckFatDuotone size={26} className="text-day" />
-        <span className="text-day text-xs">{day.tasks.filter(t=>t.checked).length+"/"+day.tasks.length}</span>
-      </div>
-    </div>
-
-    ))
-}
+    <div className="calender flex gap-6 flex-wrap mt-6">
+      {user.daysTasks.map((day) => (
+        <div
+          key={day.day}
+          className="flex-col gap-1 flex justify-start items-center"
+        >
+          <span className="text-gray-400 dark:text-gray-600 text-base">
+            {moment(day.day).format("DD")}
+          </span>
+          <div
+            className={`rounded-full h-20 w-20 font-bold text-xl text-day flex justify-center items-center flex-col gap-1 ${
+              user.todayTasks.tasks.filter((el) => !el.checked).length == 0
+                ? "bg-green-400"
+                : "bg-[#ddd] dark:bg-[#222]"
+            }`}
+          >
+            <PiCheckFatDuotone size={26} className="text-day" />
+            <span className="text-day text-xs">
+              {day.tasks.filter((t) => t.checked).length +
+                "/" +
+                day.tasks.length}
+            </span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-
